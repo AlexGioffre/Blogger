8dfb905833ded6f6b73b214eb4d48f92b2f841fd
Alex Gioffre <alexgioffre1191@gmail.com>
2019-07-08 18:18:54 +0200

    Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
   resources :articles do
     resources :comments
   end
   resources :tags
-  
+  resources :authors
   root to: 'articles#index'
 end
